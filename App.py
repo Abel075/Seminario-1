85b6be484896602cb64cf18d5ee5a47960bcdd18
Abel075 <abeld.correa@gmail.com>
2021-11-04 15:14:36 -0300

    db

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost' 
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flascontacts'
+app.config['MYSQL_DB'] = 'flaskcontacts'
 mysql = MySQL(app)
 
 # Configuraciones
